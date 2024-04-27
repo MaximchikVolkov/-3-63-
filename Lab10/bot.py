1c5ed9a20a605cafb0148622523da818dc2186e4
Maxim Volkov <user.email>
2024-04-27 13:20:10 +0300

    Add extensions

--- Lab10/bot.py
+++ Lab10/bot.py
@@ -1,26 +1,98 @@
 # -*- coding: utf-8 -*-
 import telebot
 import requests
+import json
 
-user_id = 279586664
+access_key = "c879e061-51a9-403f-9d04-45fd24ecc72e"
 telegram_token = '6828596676:AAE4M7tHHrZr40oWkyn9sr3sM3KZGG6vk4Y'
-vk_token = 'vk1.a.zlxqGwoUMLqyfkn8xYENgvz9WcBUV7O40bLtWxTm9C7mfWWIFTyDxhSg_j0IHf4FWnfmr-MdmjTaprpniH-1QXq0_HKVx4mvEhVMTx8yLEy_rdxhwxbFLHYLMTE5N9St5pK7Prv0AejoOm0WMNbwnodwIdEVYMkMJ6HHxMIetFEypCRylRriipGdT1rrr8R0lx5gQf67uI8cOYeU9CnrAA'
+
+headers = {
+    "X-Yandex-API-Key": access_key
+}
+
+query_now = """{
+  weatherByPoint(request: { lat: 55.452, lon: 37.373 }) {
+    now {
+      temperature
+    }
+  }
+}"""
+
+query_forecast = """{
+  weatherByPoint(request: { lat: 55.452, lon: 37.373 }) {
+    forecast {
+      days(limit: 1) {
+        time
+        parts {
+          morning {
+            avgTemperature
+          }
+          day {
+            avgTemperature
+          }
+          evening {
+            avgTemperature
+          }
+          night {
+            avgTemperature
+          }
+        }
+      }
+    }
+  }
+}"""
 
 tele_bot = telebot.TeleBot(telegram_token)
 
 @tele_bot.message_handler(commands=['start'])
 def start(msg):
-    tele_bot.send_message(msg.chat.id, 'Привет! Я - бот, который преобразует твои сообщения в посты ВКонтакте. Скорее напиши мне что-нибудь!')
+    user_name = msg.from_user.first_name
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(*[telebot.types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Температура сейчас']])
+    keyboard.row(*[telebot.types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Прогноз погоды']])
+    tele_bot.send_message(msg.chat.id, 'Привет, ' + user_name + '! \nЯ - бот, показывающий погоду! Могу предложить следующее:'
+                            , reply_markup=keyboard)
 
-@tele_bot.message_handler(content_types=['text'])
-def text_handler(msg):
-    text = msg.text
-    url = 'https://api.vk.com/method/wall.post'
-    data = {'close_comments': 1, 'friends_only': 1, 'access_token': vk_token, 'owner_id': user_id, 'message': text, 'v': 5.199}
-    r = requests.post(url, params=data)
-    if (r.status_code == 200):
-        tele_bot.send_message(msg.chat.id, 'Успешный успех!')
-    else:
-        tele_bot.send_message(msg.chat.id, 'Произошли какие-то неполадки(')
+@tele_bot.callback_query_handler(func=lambda msg: msg.data)
+def result(msg):
+    if msg.data == 'Температура сейчас':
+        response = requests.post('https://api.weather.yandex.ru/graphql/query', headers=headers, json={'query': query_now})
+        todos = json.loads(response.text)
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.row(*[telebot.types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Температура сейчас']])
+        keyboard.row(*[telebot.types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Прогноз погоды']])
+        tele_bot.send_message(msg.message.chat.id, 'Сейчас в Москве ' + str(todos['data']['weatherByPoint']['now']['temperature']) + ' градусов',
+                              reply_markup=keyboard)
+        
+
+    elif msg.data == 'Прогноз погоды':
+        response = requests.post('https://api.weather.yandex.ru/graphql/query', headers=headers, json={'query': query_forecast})
+        todos = json.loads(response.text)
+        morning_temp = str(todos['data']['weatherByPoint']['forecast']['days'][0]['parts']['morning']['avgTemperature'])
+        day_temp = str(todos['data']['weatherByPoint']['forecast']['days'][0]['parts']['day']['avgTemperature'])
+        evening_temp = str(todos['data']['weatherByPoint']['forecast']['days'][0]['parts']['evening']['avgTemperature'])
+        night_temp = str(todos['data']['weatherByPoint']['forecast']['days'][0]['parts']['night']['avgTemperature'])
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.row(*[telebot.types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Температура сейчас']])
+        keyboard.row(*[telebot.types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Прогноз погоды']])
+        tele_bot.send_message(msg.message.chat.id, 'Завтра в Москве будет следующая погода: \n' +
+                              'Утром - ' + morning_temp + ' градусов\n' + 
+                              'Днём - ' + day_temp + ' градусов\n' +
+                              'Вечером - ' + evening_temp + ' градусов\n' +
+                              'Ночью - ' + night_temp + ' градусов',
+                              reply_markup=keyboard)
+        
+
+# @tele_bot.message_handler(content_types=['text'])
+# def text_handler(msg):
+#     # text = msg.text
+#     # url = 'https://api.vk.com/method/wall.post'
+#     # data = {'close_comments': 1, 'friends_only': 1, 'access_token': vk_token, 'owner_id': user_id, 'message': text, 'v': 5.199}
+#     # r = requests.post(url, params=data)
+#     # if (r.status_code == 200):
+#     #     tele_bot.send_message(msg.chat.id, 'Успешный успех!')
+#     # else:
+#     #     tele_bot.send_message(msg.chat.id, 'Произошли какие-то неполадки(')
+    
 
 tele_bot.polling(none_stop=True)
--- Lab11/bot.py
+++ Lab11/bot.py
@@ -4,50 +4,73 @@ import telebot
 import xml.dom.minidom
 from datetime import date  
 from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 token = '6828596676:AAE4M7tHHrZr40oWkyn9sr3sM3KZGG6vk4Y'
 
 class TelegramBot(telebot.TeleBot):
-    select_date = ''
+    select_date_0 = ''
+    select_date_1 = ''
+    counter = 0
 
 bot = TelegramBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    bot.select_date = ''
+    bot.select_date_0 = ''
+    bot.select_date_1 = ''
     user_name = msg.from_user.first_name
-    bot.send_message(msg.chat.id, 'Привет, ' + user_name + '! Я могу показать курс валюты к рублю в выбранный день.')
+    bot.send_message(msg.chat.id, 'Привет, ' + user_name + '! Я могу показать курс валюты к рублю за выбранный период.')
     calendar, step = DetailedTelegramCalendar(max_date=date.today()).build()
-    bot.send_message(msg.chat.id, f"Выбери дату:  {LSTEP[step]}", reply_markup=calendar)
+    
+    bot.send_message(msg.chat.id, f"Выбери дату от:  {LSTEP[step]}", reply_markup=calendar)
+    bot.send_message(msg.chat.id, f"Выбери дату до:  {LSTEP[step]}", reply_markup=calendar)
+
 
 @bot.callback_query_handler(func=DetailedTelegramCalendar.func())
 def cal(c):
     result, key, step = DetailedTelegramCalendar(max_date=date.today()).process(c.data)
     if not result and key:
-        bot.edit_message_text(f"Выбери дату: {LSTEP[step]}",
+        if 'до' in c.message.text:
+            text = "Выбери дату до"
+        else:
+            text = "Выбери дату от"
+        bot.edit_message_text(f"{text}: {LSTEP[step]}",
         c.message.chat.id,
         c.message.message_id,
         reply_markup=key)
     elif result:
-        send_valute_keyboard(c)
-        bot.edit_message_text(f"Была выбрана следующая дата: {result}",
+        if 'до' in c.message.text:
+            text = "Была выбрана следующая дата до:"
+            bot.select_date_1 = str(result.strftime('%d-%m-%Y'))
+        else:
+            text = "Была выбрана следующая дата от:"
+            bot.select_date_0 = str(result.strftime('%d-%m-%Y'))
+        bot.edit_message_text(f"{text} {result}",
         c.message.chat.id,
         c.message.message_id)
-        bot.select_date = str(result.strftime('%d-%m-%Y'))
+        bot.counter+=1
+        if bot.counter == 2:
+            bot.counter = 0
+            send_valute_keyboard(c)
+        
 
 @bot.callback_query_handler(func=lambda c: c.data)
 def get_text_valute(c):
     if c.data == 'Азербайджанский манат':
-        get_value(c.message.chat.id, 'R01020A', 'Курс Азербайджанского маната - ')
+        get_value(c, 'R01020A', 'Азербайджанский манат')
     elif c.data == 'Белорусский рубль':
-        get_value(c.message.chat.id, 'R01090B', 'Курс Белорусского рубля - ')
+        get_value(c, 'R01090B', 'Белорусский рубль')
     elif c.data == 'Польский злотый':
-        get_value(c.message.chat.id, 'R01565', 'Курс Польского злотого - ')
+        get_value(c, 'R01565', 'Польский злотый')
     elif c.data == 'Казахстанский тенге':
-        get_value(c.message.chat.id, 'R01335', 'Курс Казахстанского тенге - ')
+        get_value(c, 'R01335', 'Казахстанский тенге')
     elif c.data == 'Поменять дату':
         calendar, step = DetailedTelegramCalendar(max_date=date.today()).build()
-        bot.send_message(c.message.chat.id, f"Выбери дату:  {LSTEP[step]}", reply_markup=calendar)
+        bot.send_message(c.message.chat.id, f"Выбери дату от:  {LSTEP[step]}", reply_markup=calendar)
+        bot.send_message(c.message.chat.id, f"Выбери дату до:  {LSTEP[step]}", reply_markup=calendar)
 
 def send_valute_keyboard(msg):
     keyboard = telebot.types.InlineKeyboardMarkup()
@@ -56,25 +79,35 @@ def send_valute_keyboard(msg):
     keyboard.row(*[telebot.types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Поменять дату']])
     bot.send_message(msg.message.chat.id, 'Выбери валюту:', reply_markup=keyboard)
 
-def get_value(id, valute_id, name):
-    
-    url = 'https://www.cbr.ru/scripts/XML_daily.asp?date_req=' + bot.select_date
+def get_value(msg, valute_id, name):
+    data = []
+    date = []
+    url = 'https://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=' + bot.select_date_0 + '&date_req2=' + bot.select_date_1 + '&VAL_NM_RQ=' + valute_id
     
     r = requests.get(url)
     dom = xml.dom.minidom.parseString(r.text)
     dom.normalize()
 
-    nodes = dom.getElementsByTagName("ValCurs")
-    value = nodes[0].childNodes[0].nodeValue
-
-    nodes = dom.getElementsByTagName("Valute")
-
-    for i in range(nodes.length):
-        if nodes[i].getAttribute("ID") == valute_id:
-            index = i
-            break
+    nodes = dom.getElementsByTagName("Record")
     
-    value = nodes[index].getElementsByTagName("Value")[0].childNodes[0].nodeValue
-    bot.send_message(id, name + value)
+    for i in range(nodes.length):
+        value = nodes[i].getElementsByTagName("Value")[0].childNodes[0].nodeValue
+        day = nodes[i].getAttribute("Date")
+        date.append(day)
+        data.append(value)
+    
+    for i in range(0, len(data)):
+        data[i] = float(data[i].replace(',', '.'))
+    
+    fig, ax = plt.subplots() 
+    ax.plot(date, data, color="g")
+    plt.xticks(rotation=45)
+    plt.title(name)
+    
+    plt.savefig("graph.png", bbox_inches='tight')
+    # plt.close()
+
+    bot.send_photo(msg.message.chat.id, photo=open('graph.png', 'rb'))
+    send_valute_keyboard(msg)
 
 bot.polling(none_stop=True)
